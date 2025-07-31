b7624f361298b9c4b0b497a3151861984a708ccb
chintha phanindra <chintha.phanindra40@gmail.com>
2025-07-31 22:53:57 +0530

    5build

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
+// App.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Homepage from "./components/Home";
 import Footer from "./components/Footer";
 import Contact from "./components/Contact";
 
 const App = () => {
   return (
-    <Router>
-      <div className="bg-darkBg text-white">
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <div className="bg-darkBg text-white">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 };
 
