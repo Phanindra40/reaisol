64f8906de94c1d18cb3df097bda8203a59ef4bfe
chintha phanindra <chintha.phanindra40@gmail.com>
2025-07-31 21:45:15 +0530

    Fix Contact component import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./components/Home";
-import Contact from "./components/Contact";
+import contact from "./components/contact";
 import Footer from "./components/Footer";
 
 const App = () => {
@@ -10,7 +10,7 @@ const App = () => {
       <div className="bg-darkBg text-white">
         <Routes>
           <Route path="/" element={<Homepage />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/contact" element={<contact />} />
         </Routes>
         <Footer />
       </div>
